3d669bead9c25d55e41dfd19ffd33aba96fcf6d8
Aiman <aiman.bscsf19@iba-suk.edu.pk>
2022-12-28 21:34:09 +0500

    Revert "Revert "The rest of the project.""
    This reverts commit e480cdca21bc80866faa9b4bc6bfeb1bbfa19442.

--- app/src/main/java/com/example/quizyourself/FinalScreen.kt
+++ app/src/main/java/com/example/quizyourself/FinalScreen.kt
@@ -4,27 +4,41 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class FinalScreen : AppCompatActivity() {
     lateinit var btnFinish:Button
+    lateinit var btnExit:Button
     lateinit var tvResult:TextView
     lateinit var tvUsername:TextView
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Now in","finaScreen onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_screen)
+
         btnFinish=findViewById(R.id.btn_finish)
+        btnExit=findViewById(R.id.btn_exit)
         tvResult=findViewById(R.id.tv_score)
+
         val username=intent.getStringExtra(Constants.USERNAME)
         tvUsername=findViewById(R.id.tv_username)
-        tvUsername.text = username
+        tvUsername.text = username+ ", you did good."
         val score=intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
         tvResult.text="Your score is $score out of $totalQuestions"
+
         btnFinish.setOnClickListener{
-            startActivity(Intent(this,MainActivity::class.java))
+            val intent:Intent=Intent(this,Categories::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            intent.putExtra(Constants.USERNAME,username)
+            startActivity(intent)
+            finish()
+        }
+        btnExit.setOnClickListener {
+            finish()
         }
 
     }
--- app/src/main/java/com/example/quizyourself/MainActivity.kt
+++ app/src/main/java/com/example/quizyourself/MainActivity.kt
@@ -9,18 +9,21 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nameEditText:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         val startButton:Button=findViewById(R.id.startButton)
-        val nameEditText:EditText=findViewById(R.id.et_name)
+        nameEditText=findViewById(R.id.et_name)
         startButton.setOnClickListener {
             if (nameEditText.text.toString().isEmpty()){
                 Toast.makeText(this,"Please enter your name",Toast.LENGTH_LONG).show()
             }else{
-                val intent:Intent=Intent(this,QuizQuestionsActivity::class.java)
+                val intent:Intent=Intent(this,Categories::class.java) //QuizQuestionsActivity
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 intent.putExtra(Constants.USERNAME,nameEditText.text.toString())
+
                 startActivity(intent)
                 finish()
             }
